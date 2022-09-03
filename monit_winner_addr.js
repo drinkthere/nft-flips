9fd9cb2aa850d6b6c29c75aa940c0e9e4b3eb590
willshi <willshi@silot.ai>
2022-09-03 22:30:14 +0800

    init

---
+++ monit_winner_addr.js
@@ -0,0 +1,2 @@
+const {collection} = require('minimist')(process.argv.slice(2))
+console.log(collection)
---
+++ scripts/create_table.js
@@ -0,0 +1,30 @@
+// 数据库相关
+const dbFile = __dirname + "/dbs/mint.db";
+let db;
+
+const runSql = async (sql) => {
+    db.run(sql, (err) => {
+        if (null != err) {
+            console.log(err);
+            process.exit();
+        }
+    });
+};
+
+const initDb = async () => {
+    const sqlite3 = require("sqlite3").verbose();
+    const { open } = require("sqlite");
+    db = await open({
+        filename: dbFile,
+        driver: sqlite3.Database,
+    });
+};
+
+const main = async () => {
+    await initDb();
+
+    // create table tb_address_collection
+    await runSql(
+        "CREATE TABLE tb_address_collection (id INTEGER PRIMARY KEY AUTOINCREMENT, collection VARCHAR(42), address VARCHAR(42), status INT(1), create_time DATETIME DEFAULT CURRENT_TIMESTAMP, modify_time DATETIME DEFAULT CURRENT_TIMESTAMP)"
+    );
+};
